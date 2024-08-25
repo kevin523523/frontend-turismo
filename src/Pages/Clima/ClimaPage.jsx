072e2cb05d3ee9ead1dc119fc5ae19c84c5e0e21
kevin523523 <keviprad@espol.edu.ec>
2024-08-25 16:08:27 -0500

    Cambio clima

--- src/Pages/Clima/ClimaPage.jsx
+++ src/Pages/Clima/ClimaPage.jsx
@@ -1,10 +1,18 @@
-import { Footer } from "../../Components/Footer/Footer";
-import { NavBar } from "../../Components/NavBar/NavBar";
+import { useEffect, useState } from 'react';
+import { Footer } from '../../Components/Footer/Footer';
+import { NavBar } from '../../Components/NavBar/NavBar';
+import { NavBarActive } from "../../Components/NavBar/NavBarActive"; 
 
 export const ClimaPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userId = sessionStorage.getItem('user_id');
+    setIsLoggedIn(!!userId); // Se convierte en true si `userId` existe, false en caso contrario
+  }, []);
   return (
     <div className="app-container">
-      <NavBar />
+      {isLoggedIn ? <NavBarActive /> : <NavBar />}
       <Footer />
     </div>
   )
